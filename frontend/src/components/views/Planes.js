10e1967264e9a674fc6cdd54242a5eff588c2b0c
StevenRodriguez25 <116036164+StevenRodriguez25@users.noreply.github.com>
2022-11-05 00:08:59 -0500

    Planes listos

--- frontend/src/components/views/Planes.js
+++ frontend/src/components/views/Planes.js
@@ -1,14 +1,49 @@
-import '../styles/planes.css'
-import React from 'react'
+import "../styles/planes.css";
+import React from "react";
 
-function Planes () {
-    return (
-        <div>
-            <h1>Planes</h1>
+//importancion imagenes planes
+import habitacion1 from "../../img/habitacion1.jpg";
+
+function Planes() {
+  return (
+    <div className="container">
+        <div className="card">
+            <img src={habitacion1} alt="card_image"/>
+            <h4 className="card__title">Americano</h4>
+            <h5 className="card__subtitle">Pension Completa</h5>
+            <p className="card__description">Hospedaje + Desayuno + Almuerzo + Comida</p>
+            <a href="#">Ver Disponibilidad</a>
         </div>
-    )
-
-
+        <div className="card">
+            <img className="card_image" src={habitacion1} alt="card_image"/>
+            <h4 className="card__title">Americano Modificado</h4>
+            <h5 className="card__subtitle">Variable</h5>
+            <p className="card__description">Hospedaje + Desayuno + Almuerzo o Comida</p>
+            <a href="#">Ver Disponibilidad</a>
+        </div>
+        <div className="card">
+            <img className="card_image" src={habitacion1} alt="card_image"/>
+            <h4 className="card__title">Europeo</h4>
+            <h5 className="card__subtitle">Only Estadia</h5>
+            <p className="card__description">No Incluye Alimentos ni bebidas</p>
+            <a href="#">Ver Disponibilidad</a>
+        </div>
+        <div className="card">
+            <img className="card_image" src={habitacion1} alt="card_image"/>
+            <h4 className="card__title">Continental</h4>
+            <h5 className="card__subtitle">Extra mini</h5>
+            <p className="card__description">Hospedaje + Desayuno Mini *Cafe + Pan tostado + Mermelada y Mantequilla*</p>
+            <a href="#">Ver Disponibilidad</a>
+        </div>
+        <div className="card">
+            <img className="card_image" src={habitacion1} alt="card_image"/>
+            <h4 className="card__title">*All-IN*</h4>
+            <h5 className="card__subtitle">Completo</h5>
+            <p className="card__description">Hospedaje + Comida + Entretenimiento</p>
+            <a href="#">Ver Disponibilidad</a>
+        </div>
+    </div>
+  );
 }
 
 export default Planes;
